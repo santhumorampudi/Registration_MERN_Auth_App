15cb4eabb03df398b163d49b7795cd8135320d8b
santhumorampudi <74274595+santhumorampudi@users.noreply.github.com>
2020-12-10 06:40:35 -0800

    Add files via upload

---
+++ registrationApp/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://new-user_1:new-user_1@loggedusers.pjcoy.mongodb.net/loggedUsers?retryWrites=true&w=majority" ,
+    secretOrKey: "secret"
+  };
---
+++ registrationApp/config/passport.js
@@ -0,0 +1,26 @@
+/*breaking down how the JWT authentication strategy is constructed, 
+explaining required parameters, variables and functions such as options, 
+secretOrKey, jwtFromRequest, verify, and jwt_payload.*/
+
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../config/keys");
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
